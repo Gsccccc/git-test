dda6bb914434eca17b3c84bfd878544c832db014
GSC <1106467982@qq.com>
2022-02-24 15:55:34 +0800

    first comment

---
+++ src/main/java/com/gsc/git/gitTest.java
@@ -0,0 +1,13 @@
+package com.gsc.git;
+
+/**
+ * @progrom:git-test
+ * @description:
+ * @author:作者
+ * @create:2022-02-24 15:51
+ */
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
