aa370d2b52d9f6ae863a7bf3c293b08258c4409b
GSC <1106467982@qq.com>
2022-02-24 16:15:38 +0800

    hot-fix first comment

--- src/main/java/com/gsc/git/gitTest.java
+++ src/main/java/com/gsc/git/gitTest.java
@@ -9,5 +9,9 @@ package com.gsc.git;
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
+        System.out.println("hello git3!");
+        System.out.println("hello git4!");
+        System.out.println("hello git5!");
     }
 }
